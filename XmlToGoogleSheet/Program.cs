481cfac835302d58bbdb61aa12fa1c5d3a419dc3
Shahi3d <sh.bagdeli@live.com>
2023-05-09 13:29:20 +0330

    Created a simple sample code to test the code

--- XmlToGoogleSheet/Program.cs
+++ XmlToGoogleSheet/Program.cs
@@ -1,2 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.Xml;
+
 Console.WriteLine("Hello, World!");
+
+XmlDocument doc = new XmlDocument();
+doc.PreserveWhitespace = true;
+try { doc.Load("example.xml"); }
+catch (System.IO.FileNotFoundException ex)
+{
+    Console.WriteLine("File Not Found!");
+    Console.WriteLine(ex.Message);
+}
+
+if(doc.DocumentElement != null)
+{
+    Console.Write(doc.DocumentElement.OuterXml);
+}
