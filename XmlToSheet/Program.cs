2c82122d7aba36025ba5cfd21554f5de63ed6e57
Shahi3d <sh.bagdeli@live.com>
2023-05-12 19:28:06 +0330

    Writed a simple xml reader that will print the xml to console

--- XmlToSheet/Program.cs
+++ XmlToSheet/Program.cs
@@ -5,7 +5,15 @@ Console.WriteLine("Hello, World!");
 
 XmlDocument doc = new XmlDocument();
 doc.PreserveWhitespace = true;
-try { doc.Load("example.xml"); }
+
+var docsName = "example.xml";
+
+if(args.Length > 0)
+{
+    docsName = args[0];
+}
+
+try { doc.Load(docsName); }
 catch (System.IO.FileNotFoundException ex)
 {
     Console.WriteLine("File Not Found!");
@@ -15,4 +23,34 @@ catch (System.IO.FileNotFoundException ex)
 if(doc.DocumentElement != null)
 {
     Console.Write(doc.DocumentElement.OuterXml);
+
+    Console.WriteLine();
+
+    if (doc.HasChildNodes)
+    {
+        for (int i = 0; i < doc.ChildNodes.Count; i++)
+        {
+            XmlNode child = doc.ChildNodes[i];
+            if(child !=null && child.HasChildNodes)
+            {
+                for (int j = 0; j < child.ChildNodes.Count; j++)
+                {
+                    var childNode = child.ChildNodes[j];
+                    var outer = childNode.OuterXml.Replace("\r", "").Replace("\n", "").ToString();
+                    if (!string.IsNullOrWhiteSpace(outer))
+                    {
+                        Console.WriteLine(outer);
+                    }
+
+                    var text = childNode.InnerText.Replace("\r", "").Replace("\n", "").ToString();
+                    if (!string.IsNullOrWhiteSpace(text))
+                    {
+                        Console.WriteLine($"Node Name: {childNode.Name}");
+                        Console.WriteLine(text);
+                    }
+                }
+            }
+        }
+    } 
 }
+
